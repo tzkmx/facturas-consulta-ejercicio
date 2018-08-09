0802dc69ce17fcc8a7610cef40b250698924dd8b
Jesus E. Franco Martinez <tezcatl@fedoraproject.org>
2018-08-08 23:23:17 -0500

    Probando diferentes registros de consultas

--- tests/Unit/RequestHandler/ValidateYearIsCompleteTest.php
+++ tests/Unit/RequestHandler/ValidateYearIsCompleteTest.php
@@ -49,23 +49,17 @@ class ValidateYearIsCompleteTest extends TestCase
         $this->assertEquals($expectedValidRequest, $validatedRequest);
     }
 
-    public function testSuccessQueriesDontCoverWholeYear()
+    /**
+     * @dataProvider getBadRangesToMarkIncomplete
+     */
+    public function testSuccessQueriesDontCoverWholeYear(array $queries)
     {
         $noPendingQueriesButBadRange = [
             'clientId' => 'testing',
             'year' => '2017',
             'pendingQueries' => [],
-            'queriesFetched' => 1,
-            'successQueries' => [
-                [
-                    'range' => [
-                        'start' => '2017-01-01',
-                        'finish' => '2017-12-30',
-                    ],
-                    'tries' => 1,
-                    'billsIssued' => 99,
-                ],
-            ],
+            'queriesFetched' => count($queries),
+            'successQueries' => $queries,
         ];
         $validator = new ValidateYearIsComplete();
 
@@ -74,21 +68,41 @@ class ValidateYearIsCompleteTest extends TestCase
             'year' => '2017',
             'isComplete' => false,
             'pendingQueries' => [],
-            'queriesFetched' => 1,
-            'successQueries' => [
-                [
-                    'range' => [
-                        'start' => '2017-01-01',
-                        'finish' => '2017-12-30',
-                    ],
-                    'tries' => 1,
-                    'billsIssued' => 99,
-                ],
-            ],
+            'queriesFetched' => count($queries),
+            'successQueries' => $queries,
         ];
 
         $validatedRequest = $validator->handleRequest($noPendingQueriesButBadRange);
 
         $this->assertEquals($expectedIncompleteRequest, $validatedRequest);
+    }
+
+    public function getBadRangesToMarkIncomplete()
+    {
+        return [
+            '1 query, 364 days' => [
+                [
+                    [
+                        'range' => [ 'start' => '2017-01-01', 'finish' => '2017-12-30' ],
+                        'tries' => 1,
+                        'billsIssued' => 99,
+                    ]
+                ]
+            ],
+            '2 queries, 364 days' => [
+                [
+                    [
+                        'range' => [ 'start' => '2017-01-01', 'finish' => '2017-01-31' ],
+                        'tries' => 1,
+                        'billsIssued' => 10,
+                    ],
+                    [
+                        'range' => [ 'start' => '2017-02-01', 'finish' => '2017-12-30' ],
+                        'tries' => 1,
+                        'billsIssued' => 20,
+                    ]
+                ]
+            ]
+        ];
     }
 }
